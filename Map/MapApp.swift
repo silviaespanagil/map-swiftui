b0b2cb5c29d635a329339c93f49aaee56feb6183
Silvia España <silviaespanagil@gmail.com>
2022-01-10 12:11:49 +0100

    Set up repository

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Silvia España on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
