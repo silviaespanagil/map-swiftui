4c245f6b0ee1e0c8af70903481f6caa16a930004
Silvia España <silviaespanagil@gmail.com>
2022-01-10 17:21:52 +0100

    Update buttonStyle to take environment variable

--- Map/Views/SearchButton.swift
+++ Map/Views/SearchButton.swift
@@ -10,22 +10,24 @@ import SwiftUI
 struct SearchButton: ButtonStyle {
     
     typealias Body = Button
-
+    
+    @Environment(\.isEnabled)  var isEnabled: Bool
+    
     func makeBody(configuration: Self.Configuration) -> some View {
         
         if configuration.isPressed {
             return configuration
                 .label
                 .padding()
-                .background(Color("Silver"))
-                .foregroundColor(Color("LightRose"))
+                .background(isEnabled ? Color("LightRose") : Color.clear)
+                .foregroundColor(isEnabled ? Color("Green") : Color.clear)
                 .clipShape(RoundedRectangle(cornerRadius: 10))
         } else {
             return configuration
                 .label
                 .padding()
-                .background(Color("Green"))
-                .foregroundColor(Color("LightRose"))
+                .background(isEnabled ? Color("Green") : Color.clear)
+                .foregroundColor(isEnabled ? Color("LightRose") : Color.clear)
                 .clipShape(RoundedRectangle(cornerRadius: 10))
         }
     }
