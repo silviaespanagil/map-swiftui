6c0b2f2b394f037161df76a33c4506c8f3e6f17a
Silvia España <silviaespanagil@gmail.com>
2022-01-21 12:54:05 +0100

    Add a color extension to manage customized colours

---
+++ Map/ColorExtension.swift
@@ -0,0 +1,19 @@
+//
+//  ColorExtension.swift
+//  Map
+//
+//  Created by Silvia España on 20/1/22.
+//
+
+import Foundation
+import SwiftUI
+
+public extension Color {
+
+    // By colours
+    static let wine = Color("Wine")
+    static let burgundy = Color("Burgundy")
+    static let green = Color("Green")
+    static let ligthRose = Color("LightRose")
+    static let silver = Color("Silver")
+}
